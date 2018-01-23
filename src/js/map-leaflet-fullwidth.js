72c985a91f84d69fc5f89fe475b8d205265eee77
Emma O'Neill <emmaruthoneill@gmail.com>
2018-01-22 17:31:17 -0800

    copy edit changes

--- src/js/map-leaflet-fullwidth.js
+++ src/js/map-leaflet-fullwidth.js
@@ -9,12 +9,12 @@ var formatthousands = d3.format(",");
 function formatDate(date,monSTR) {
   var hours = date.getHours();
   var minutes = date.getMinutes();
-  var ampm = hours >= 12 ? 'pm PDT' : 'am PDT';
+  var ampm = hours >= 12 ? 'p.m. PST' : 'a.m. PST';
   hours = hours % 12;
   hours = hours ? hours : 12; // the hour '0' should be '12'
   minutes = minutes < 10 ? '0'+minutes : minutes;
   var strTime = hours + ':' + minutes + ' ' + ampm;
-  return monSTR + ". " + date.getDate() + ", " + date.getFullYear() + " at " + strTime;
+  return monSTR + ". " + date.getDate() + ", " + date.getFullYear() + ", at " + strTime;
 }
 
 // setting parameters for the center of the map and initial zoom level
